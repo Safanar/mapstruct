7782bf506aad9243a44ac3f736d0758d76a14df6
Cheng-En Chung <satan251008@gmail.com>
2020-06-19 09:29:33 +0800

    Update NoMappingMethod.java

--- core/src/main/java/org/mapstruct/control/NoMappingMethod.java
+++ core/src/main/java/org/mapstruct/control/NoMappingMethod.java
@@ -3,6 +3,11 @@ package org.mapstruct.control;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * The interface No mapping method.
+ * Created on Jun, 19, 2020 09:29:16 AM CST
+ * @author Vayne Chung
+ */
 @Retention(RetentionPolicy.CLASS)
 @MappingControls({@MappingControl(MappingControl.Use.DIRECT), @MappingControl(MappingControl.Use.BUILT_IN_CONVERSION)})
 public @interface NoMappingMethod {
